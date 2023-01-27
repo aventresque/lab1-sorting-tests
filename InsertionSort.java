7a12f77ac8399527b66a56952953c193ebc37493
Anthony <anthony.ventresque@ucd.ie>
2023-01-27 23:41:43 +0000

    nothing to report

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,20 +1,19 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class InsertionSort {
-  void InsertionSort(List<Integer> nums){
-    for(int i = 1; i < nums.size(); i++){
-        int value = nums.get(i);
-        int j = i - 1;
-        while(j >= 0 && nums.get(j) > value){
-        nums.set(j+1, nums.get(j));
-        j = j - 1;
+    void InsertionSort(List<Integer> nums){
+        for(int i = 1; i < nums.size(); i++){
+            int value = nums.get(i);
+            int j = i - 1;
+            while(j >= 0 && nums.get(j) > value){
+            nums.set(j+1, nums.get(j));
+            j = j - 1;
+            }
+            nums.set(j+1, value);
         }
-        nums.set(j+1, value);
     }
-  }
     
     static List<Integer> generateRandomArray(int n){
         ArrayList<Integer> list = new ArrayList<Integer>(n);
@@ -27,7 +26,6 @@ public class InsertionSort {
     return list;
     }  
 
-	// Method to test above
     public static void main(String args[])
     {
         InsertionSort ob = new InsertionSort();
